21056922a1086f95ab86e48c23593ad57ff175fb
SyedMaazAhmed47 <2023F-BIT-023@ssuet.edu.pk>
2026-05-01 01:58:59 +0500

    Update home route to reflect AWS ECS and S3

--- app/app.py
+++ app/app.py
@@ -4,13 +4,18 @@ import os
 
 app = Flask(__name__)
 
+S3_BUCKET = "sna-static-files"
+S3_REGION = "us-east-1"
+S3_IMAGE_URL = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/logo.png"
+
 @app.route("/")
 def home():
     hostname = socket.gethostname()
     return f"""
-    <h1> SNA LAB Project</h1>
+    <h1>Flask App on AWS ECS Fargate</h1>
     <p><b>Served by Pod:</b> {hostname}</p>
-    <p><b>Environment:</b> Kubernetes</p>
+    <p><b>Environment:</b> AWS ECS + S3</p>
+    <img src="{S3_IMAGE_URL}" alt="Logo from S3" width="200"/>
     """
 
 @app.route("/health")
